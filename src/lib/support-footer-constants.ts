b28647efb8cf407c1fafbb4d95306d120fdc8bb7
javier Palencia <javi.web23@gmail.com>
2026-06-27 22:32:06 -0400

    feat: footer movible

---
+++ src/lib/support-footer-constants.ts
@@ -0,0 +1,6 @@
+export const SUPPORT_WHATSAPP_URL =
+  "https://wa.me/584121591072?text=Hola,%20registr%C3%A9%20mi%20n%C3%BAmero%20[Escribe%20tu%20n%C3%BAmero]%20en%20Huellas%20a%20Salvo%20y%20perd%C3%AD%20mi%20enlace%20de%20edici%C3%B3n.%20%C2%BFMe%20ayudas%20a%20recuperarlo%3F";
+
+/** Compensa la altura de la barra fija de WhatsApp en el layout principal. */
+export const FIXED_SUPPORT_BAR_PADDING =
+  "pb-[calc(5rem+env(safe-area-inset-bottom,0px))] sm:pb-[calc(4rem+env(safe-area-inset-bottom,0px))]";
